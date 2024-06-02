c8682862ff90e66e7db547405112d944c837122f
thattechgirlie <wanji.njoro1@gmail.com>
2024-06-02 09:03:03 -0700

    web dynamic

---
+++ web_dynamic/4-hbnb.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+integrates with AirBnB static HTML Template
+"""
+import uuid
+from flask import Flask, render_template, url_for
+from models import storage
+
+# flask setup
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
+
+
+# begin flask page rendering
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/4-hbnb/')
+def hbnb_filters(the_id=None):
+    """
+    handles states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    cache_id = (str(uuid.uuid4()))
+    return render_template('4-hbnb.html',
+                           states=states,
+                           amens=amens,
+                           places=places,
+                           users=users,
+                           cache_id=cache_id)
+
+
+if __name__ == "__main__":
+    """ this is the main app"""
+    app.run(host=host, port=port)
